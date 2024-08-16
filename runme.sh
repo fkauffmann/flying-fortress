3dd77dfd9a64de5b702bdfb372def0ed8bf95283
Fabrice Kauffmann <fabrice.kauffmann@gmail.com>
2024-08-16 18:04:08 +0200

    Bash script runme.sh added

---
+++ runme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remplacez /chemin/vers/venv par le chemin vers votre environnement virtuel
+source ./.venv/bin/activate
+
+# Remplacez /chemin/vers/game.py par le chemin vers votre programme Python
+python3 game.py
+
+# Désactive l'environnement virtuel après l'exécution du script
+source ./.venv/bin/activate
