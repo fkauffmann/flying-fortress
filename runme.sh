db1c7d64bc67559bbfcdea4a5754af893d9328e1
fkauffmann <fabrice.kauffmann@gmail.com>
2026-06-19 12:58:42 +0200

    Fixed end-of-line character in runme.sh

--- runme.sh
+++ runme.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# Remplacez /chemin/vers/venv par le chemin vers votre environnement virtuel
 source ./.venv/bin/activate
-
-# Remplacez /chemin/vers/game.py par le chemin vers votre programme Python
 python3 game.py
-
-# Désactive l'environnement virtuel après l'exécution du script
-source ./.venv/bin/activate
